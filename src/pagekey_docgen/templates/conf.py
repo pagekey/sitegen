dace4a185e832379e829deb30a7771e796bfdad6
Steve G (Streaming) <pagekeystreaming@gmail.com>
2024-02-04 12:44:52 -0500

    Add first pass at Sphinx template files

---
+++ src/pagekey_docgen/templates/conf.py
@@ -0,0 +1,28 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'PROJECT_NAME_HERE'
+copyright = '2024, AUTHOR_NAME_HERE'
+author = 'AUTHOR_NAME_HERE'
+release = 'PROJECT_RELEASE_HERE'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = []
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
