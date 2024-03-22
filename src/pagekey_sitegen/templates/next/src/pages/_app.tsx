c5c4f2d427f82aaf74b7dded6f78a3b17930e2e1
Stephen Grice <stephengrice@live.com>
2024-03-21 22:39:17 -0400

    fix: Add Mantine to Next template

--- src/pagekey_sitegen/templates/next/src/pages/_app.tsx
+++ src/pagekey_sitegen/templates/next/src/pages/_app.tsx
@@ -1,11 +1,17 @@
+import { createTheme, MantineProvider } from '@mantine/core';
 import Layout from '@/lib/Layout'
 import '@/styles/globals.css'
+import '@mantine/core/styles.css';
 import type { AppProps } from 'next/app'
+
+const theme = createTheme({});
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <MantineProvider theme={theme}>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </MantineProvider>
   )
 }
---
+++ test/integration/next/about.tsx
@@ -0,0 +1,11 @@
+import { Button } from '@mantine/core';
+
+
+export default function AboutPage() {
+    return (
+        <>
+            All about this page.
+            Here is a Mantine button: <Button>Button!</Button>
+        </>
+    )
+}
